8d78b4e17a7d6e4c1395d80c8d3d1156459a469e
HugoEC1 <90981165+HugoEC1@users.noreply.github.com>
2026-01-07 00:56:08 -0500

    Add JsonExporter node for exporting scene data
    Introduces a new JsonExporter class in Godot with exported references to players and ball nodes. This sets up the structure for exporting scene information to JSON.
    ^
    yeah that

---
+++ bobblefuck/Scenes/JsonExporter.cs
@@ -0,0 +1,14 @@
+using Godot;
+using System;
+
+public partial class JsonExporter : Node
+{
+    // andrew access these classes and store info in json
+    [Export] Node players;
+    [Export] RigidBody3D ball;
+
+    void Export() {
+        // do yo shit
+        
+    }
+}
