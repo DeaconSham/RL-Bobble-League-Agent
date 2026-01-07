568194498efde9a0fb6ff974770a3953d54dbb11
aqiu266 <andrew.qiu266@gmail.com>
2026-01-07 00:34:13 -0500

    created unfinished python script to read position from scene.tscn

---
+++ get_position_script.py
@@ -0,0 +1,22 @@
+import os
+cur_path = os.path.realpath(__file__)
+cur_dir = os.path.dirname(cur_path)
+
+scene_f = open(cur_dir + "\\bobblefuck\\scene.tscn", "r")
+file_array = []
+line = scene_f.readline()
+
+while line != '':
+    file_array.append(line)
+    line = scene_f.readline()
+
+pos_ball = []
+
+for line in file_array:
+    if line[:17] == '[node name=\"Ball\"':
+        # some regex to come up with ball_extract
+        # pos_ball = extracted_ball_pos
+        pass
+
+if pos_ball == []:
+    print('you goofed')
